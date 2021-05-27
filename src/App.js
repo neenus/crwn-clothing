0d6b15da1c698f1ae124929d81b2fc6bcef22e1a
Neenus Gabriel <hello@neenus.com>
2021-05-27 12:07:38 -0400

    feat(directory.component.jsx menu-item.component.jsx): setup react router
        update menu items to navigate using react withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 import HomePage from "./pages/homepage/hompage.component";
+
+const HatsPage = () => <h1>Hats page</h1>;
+const JacketsPage = () => <h1>Jackets page</h1>;
+const SneakersPage = () => <h1>Sneakers page</h1>;
+const MenPage = () => <h1>Men's page</h1>;
+const WomenPage = () => <h1>Women's page</h1>;
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/jackets" component={JacketsPage} />
+        <Route exact path="/sneakers" component={SneakersPage} />
+        <Route exact path="/mens" component={MenPage} />
+        <Route exact path="/womens" component={WomenPage} />
+      </Switch>
     </div>
   );
 }
